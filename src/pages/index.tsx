9c9c1439958d6a2bf95844ede064e94a27f2287d
yoons0717 <yoons0717@gmail.com>
2022-09-11 17:10:30 +0900

    setting config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react'
+import { Link } from 'gatsby'
+
+const IndexPage: FunctionComponent = function () {
+  return (
+    <>
+      <Link to="/info/">To Info</Link>
+    </>
+  )
+}
+
+export default IndexPage
