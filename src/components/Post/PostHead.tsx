97d16e34b8c3b20b3b220e4b3dfd3fbb9caf155b
yoons0717 <yoons0717@gmail.com>
2022-12-11 21:10:33 +0900

    Add PostHeadInfo & responsive style

--- src/components/Post/PostHead.tsx
+++ src/components/Post/PostHead.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
+import PostHeadInfo from './PostHeadInfo'
 
 type GatsbyImgProps = {
   image: IGatsbyImageData
@@ -8,7 +9,7 @@ type GatsbyImgProps = {
   className?: string
 }
 
-type PostHeadProps = {
+type PostHeadProps = PostHeadInfoProps & {
   thumbnail: IGatsbyImageData
 }
 
@@ -16,6 +17,10 @@ const PostHeadWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const BackgroundImage = styled((props: GatsbyImgProps) => (
@@ -26,12 +31,22 @@ const BackgroundImage = styled((props: GatsbyImgProps) => (
   height: 400px;
   object-fit: cover;
   filter: brightness(0.25);
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
-const PostHead: FunctionComponent<PostHeadProps> = function ({ thumbnail }) {
+const PostHead: FunctionComponent<PostHeadProps> = function ({
+  title,
+  date,
+  categories,
+  thumbnail,
+}) {
   return (
     <PostHeadWrapper>
       <BackgroundImage image={thumbnail} alt="thumbnail" />
+      <PostHeadInfo title={title} date={date} categories={categories} />
     </PostHeadWrapper>
   )
 }
