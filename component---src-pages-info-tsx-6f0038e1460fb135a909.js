4a046f9c7410c9770cce5ef9b95c3c28427827e9
yoons0717 <yoons0717@gmail.com>
2022-12-19 22:52:07 +0900

    Updates

---
+++ component---src-pages-info-tsx-6f0038e1460fb135a909.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[977],{8187:function(t,e,a){a.r(e);var s=a(3431);var r={name:"y24uj",styles:"*{margin:0;padding:0;box-sizing:border-box;font-size:20px;}"},i={name:"z9uu1g",styles:"font-size:18px;font-weight:700;color:gray"};e.default=function(t){var e=t.data.site.siteMetadata,a=e.title,n=e.description,u=e.author;return(0,s.tZ)("div",null,(0,s.tZ)(s.xB,{styles:r}),(0,s.tZ)("div",{css:i},a),n," ",u)}}}]);
+//# sourceMappingURL=component---src-pages-info-tsx-6f0038e1460fb135a909.js.map
